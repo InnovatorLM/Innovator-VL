e71884b7c7fbdee53ed38a82cad9bcacb4ed54cd
Zichen Wen <Zichen.Wen@outlook.com>
2026-01-26 21:20:55 +0800

    update

--- RL/dataset/preprocess/__init__.py
+++
@@ -1,2 +0,0 @@
-def get_preprocess_func(dataset_path: str):
-    return lambda x: x
--- RL/dataset/prompt.py
+++
@@ -1,5 +0,0 @@
-from .const import ProblemType
-
-PROBLEM_TYPE_SPECIAL_PROMPT = {
-    str(ProblemType.SPATIAL_REASONING): " Please first generate the coordinates (x, y, x, y) of each relevant object mentioned in the problem.\n"
-}
